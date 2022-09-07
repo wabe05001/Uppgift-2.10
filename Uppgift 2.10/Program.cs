1bb8e95ed839cffd2b3f912c5530e6fbef33717a
EmilWallett <nhlpool27@gmail.com>
2022-09-07 11:56:47 +0200

    Add project files.

---
+++ Uppgift 2.10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Uppgift_2_1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("vad är din favorit bok?");
+            string harrypotter = Console.ReadLine();
+            Console.WriteLine(harrypotter);
+
+
+        }
+
+    }
+
+
+
+
+
+}
